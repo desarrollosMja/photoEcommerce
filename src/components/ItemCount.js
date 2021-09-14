accf43ff99343f02e6a73012434c7736e0274e9f
Desarrollos MJA <desarrollos.mja@gmail.com>
2021-09-13 21:47:16 -0300

    mejore eficiencia de funciones de contador

--- src/components/ItemCount.js
+++ src/components/ItemCount.js
@@ -3,23 +3,27 @@ import { useState } from "react"
 const ItemCount = ({stock, initial, onAdd}) => {
     
     const [contador, setContador] = useState(initial)
+
+    function aumentarContador(){
+        if (contador < stock){
+            setContador(contador+1)
+        }
+    }
+
+    function reducirContador(){
+        if (contador > 0){
+            setContador(contador-1)
+        }
+    }
     
     return (
         <>
             <div id="item">
-                <button id="incrementar" onClick={() => {
-                        if (contador < stock){
-                            setContador(contador+1)
-                        }
-                    }}>
+                <button id="incrementar" onClick={() => aumentarContador()}>
                     +
                 </button>
                 <p id="contadorPlace">{contador}</p>
-                <button id="decrementar" onClick={() => {
-                        if (contador > 0){
-                            setContador(contador-1)
-                        }
-                    }}>
+                <button id="decrementar" onClick={() => reducirContador()}>
                     -
                 </button><br></br>
                 <button onClick={onAdd}>Agregar al carrito</button>
