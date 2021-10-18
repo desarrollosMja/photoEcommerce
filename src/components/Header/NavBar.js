222632941e13389dde0af1d03eb239aa984c849f
Desarrollos MJA <desarrollos.mja@gmail.com>
2021-10-18 16:31:26 -0300

    Presentacion final del proyecto

--- src/components/Header/NavBar.js
+++ src/components/Header/NavBar.js
@@ -2,7 +2,7 @@ import CartWidget from "./CartWidget"
 import Navbar from "react-bootstrap/Navbar"
 import Container from "react-bootstrap/Container"
 import Nav from "react-bootstrap/Nav"
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 
 const NavBar = () => {
 
@@ -11,15 +11,15 @@ const NavBar = () => {
         <Navbar bg="dark" variant="dark">
             <Container className="align-items-center">
                 <Container>
-                    <Link to="/">
+                    <NavLink to="/">
                         <h1>El obturador</h1>
                         <h3 className="fs-5">Tienda de fotografía</h3>
-                    </Link>
+                    </NavLink>
                 </Container>
                 <Nav className="me-auto ms-5">
-                    <Link className="nav-link" to={"/categoria/1"}>Cámaras de foto</Link>
-                    <Link className="nav-link" to={"/categoria/2"}>Accesorios</Link>
-                    <Link className="nav-link" to={"/categoria/3"}>Cámaras de video</Link>
+                    <NavLink className="nav-link" to={"/categoria/1"}>Cámaras de foto</NavLink>
+                    <NavLink className="nav-link" to={"/categoria/2"}>Accesorios</NavLink>
+                    <NavLink className="nav-link" to={"/categoria/3"}>Cámaras de video</NavLink>
                 </Nav>
                 <CartWidget/>
             </Container>
